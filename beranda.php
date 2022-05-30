70ed9e74211d12c1e3175196f54a05da723739cc
Farhan <farhanzuhair040703@gmail.com>
2022-05-30 08:21:25 +0700

    minor update

--- beranda.php
+++ beranda.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style/beranda.css">
 
-    <title>Obatin | Home</title>
+    <title>Beranda</title>
   </head>
   <body>
     <section>
